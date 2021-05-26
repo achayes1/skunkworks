b952480caa1cc465f4db0b383e87091f5e114c24
achayes1 <andrew.c.hayes1@gmail.com>
2021-05-25 21:26:43 -0500

    update

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@
            }}>
            <Section title="Step One">
              Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
+             screen and then come back to see your SAMPLE.
            </Section>
            <Section title="See Your Changes">
              <ReloadInstructions />
